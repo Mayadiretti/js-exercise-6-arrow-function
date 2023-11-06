198759c81860a0b57a5480ff298181be55d342bc
maya diretti <maya.diretti@icloud.com>
2023-11-06 17:57:18 +0100

    exercise 1

---
+++ main.js
@@ -0,0 +1,5 @@
+/*Crea una arrow functio che accetta due numeri come parametri e restituisce la loro sottrazione*/
+const sottrazione = (num1, num2) => {
+    console.log(num1 - num2);
+};
+const result = sottrazione (10, 3);
